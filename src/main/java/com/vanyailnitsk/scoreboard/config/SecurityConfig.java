8a6672b7fc2dde69a6ea7651df0e42ff302d1bec
Vanya <vanyailnitsk@yandex.ru>
2023-05-11 20:06:39 +0300

    string origins

--- src/main/java/com/vanyailnitsk/scoreboard/config/SecurityConfig.java
+++ src/main/java/com/vanyailnitsk/scoreboard/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("http://46.19.66.154:3000/"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
